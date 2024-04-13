365f9a5eeee1cbf2bf002156d237fb8b8512f7e5
Prokopevs <prokopev_savva@mail.ru>
2024-04-13 16:28:05 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,18 @@ import { useEffect, useRef, useState } from 'react';
 function App() {
   const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
   
-  if (isMobile && (width < 800)) {
+  // if (isMobile && (width < 800)) {
     return (
       <>
         <AppRouter />
       </>
     )
-  }  
+  // }  
 
-  return (
-  <div className='dontMobile'> 
-    <p className='dontMobile_text'>Please use mobile</p>
-  </div>
+  // return (
+  // <div className='dontMobile'> 
+  //   <p className='dontMobile_text'>Please use mobile</p>
+  // </div>
 )
 }
 // isMobileNavigator()
