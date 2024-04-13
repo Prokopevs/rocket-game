7c42beb3d5b8daefa1603194e3e4829b25d7ef86
Prokopevs <prokopev_savva@mail.ru>
2024-04-13 16:46:53 +0300

    /

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,21 @@ import { isMobileNavigator } from './lib/helpers';
 import { useEffect, useRef, useState } from 'react';
 
 function App() {
-  // const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
   
-  // if (isMobile && (width < 800)) {
+  if (isMobile && (width < 800)) {
     return (
       <>
         <AppRouter />
       </>
     )
-  // }  
+  }  
 
-  // return (
-  // <div className='dontMobile'> 
-  //   <p className='dontMobile_text'>Please use mobile</p>
-  // </div>
-// )
+  return (
+  <div className='dontMobile'> 
+    <p className='dontMobile_text'>Please use mobile</p>
+  </div>
+)
 }
 // isMobileNavigator()
 export default App;
